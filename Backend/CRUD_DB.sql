9e0b061cfb222122b6b739c4c22d24a3f2d155c4
djgarciaa841-lab <djgarciaa841@gmail.com>
2026-06-19 20:15:23 -0500

    agregar DB para el Backend

--- Backend/CRUD_DB.sql
+++ Backend/CRUD_DB.sql
@@ -1,19 +1,43 @@
 CREATE DATABASE crud_db;
-USE DATABASE crud_db;
+USE crud_db;
 
-CREATE TABLE users (
+CREATE TABLE usuarios (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
+    nombre VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
-)
+CREATE TABLE productos (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(100) NOT NULL,
+    precio DECIMAL(10,2) NOT NULL,
+    imagen_url VARCHAR(255) NOT NULL,
+    categoria VARCHAR(100) NOT NULL,
+    descuento DECIMAL(5,2) DEFAULT NULL
+);
+
+CREATE TABLE carrito (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    usuario_id INT NOT NULL,
+    producto_id INT NOT NULL,
+    cantidad INT NOT NULL,
+    fecha_agregado TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
+    FOREIGN KEY (producto_id) REFERENCES productos(id)
+ 
+);
 
 CREATE TABLE contactanos (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    nombre VARCHAR(255) NOT NULL,
-    apellido VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    telefono VARCHCAR(255) NOT NULL
-)
+    nombre VARCHAR(100) NOT NULL,
+    apellido VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    codigo_pais VARCHAR(20) NOT NULL,
+    telefono VARCHAR(255) NOT NULL,
+    comentarios TEXT,
+    fecha_envio TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
 
